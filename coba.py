092b1870628f05440a7c776ece36bba69fae68d1
Inthumaktics <inthumakticss@gmail.com>
2025-06-07 23:05:11 +0700

    Membuat folder studi kasus dan file studi kasus css

---
+++ coba.py
@@ -0,0 +1,12 @@
+def level_up (kamu):
+    sekarang = kamu
+
+    while hidup():
+        taklukkan_ketakutan()
+        bangun_kekuatan()
+
+        if gagal():
+            bangkit()
+        
+        KeluardariZonaNyaman()
+        curent += 0.1
